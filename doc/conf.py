89a016c272b6cc8dccf1d118aba606ea7e54b8db
Francesco De Carlo <decarlof@gmail.com>
2016-03-07 15:38:54 -0600

    fixed for readthedocs to compile

--- doc/conf.py
+++ doc/conf.py
@@ -308,11 +308,16 @@ class Mock(object):
 
     @classmethod
     def __getattr__(cls, name):        return Mock()    def __mul__(self, other):        return Mock()    def __rmul__(self, other):        return Mock()    def __pow__(self, other):        return Mock()    def __div__(self, other):        return Mock()    def __add__(self, other):        return Mock()    def __radd__(self, other):        return Mock()            
-#  'ConfigParser', 
+#MOCK_MODULES = [
+#    'pyinotify', 'validate_email', 'subprocess', 'argparse',
+#    'fnmatch', 'suds', 'logging', 'traceback', 'urllib2',
+#    'httplib', 'xml', 'ipdb', 'collections', 'unicodedata', 'string']
+
 MOCK_MODULES = [
-    'pyinotify', 'validate_email', 'subprocess', 'argparse',
-    'fnmatch', 'suds', 'logging', 'traceback', 'urllib2',
-    'httplib', 'xml', 'ipdb', 'collections', 'unicodedata', 'string']
+    'numpy', 'scipy', 'scipy.misc', 'scipy.optimize', 'h5py', 'ctypes',
+    'matplotlib', 'matplotlib.pylab', 'tifffile', 'EdfFile', 'netCDF4',  
+    'spefile', 'scipy.ndimage', 'pywt', 'scikit-image', 'skimage',
+    'skimage.io', 'skimage.filter', 'skimage.morphology', 'DM3lib']
 
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
