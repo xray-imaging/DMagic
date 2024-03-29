a1f8dae517a7ad2ffb7961a869b45cd7ea514f39
Francesco De Carlo <decarlof@gmail.com>
2024-03-29 18:33:53 -0500

    added logo

--- doc/conf.py
+++ doc/conf.py
@@ -139,8 +139,8 @@ html_theme_options = {
     'logo_only': True,
 }
 
-html_logo = 'img/dmagic-logo.png'
-html_favicon = 'img/dmagic-logo.png'
+html_logo = 'source/img/dmagic-logo.png'
+html_favicon = 'source/img/dmagic-logo.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
